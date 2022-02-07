8b1b9007970d7a27fbf87639efae0496dee5b40b
Bijoy Biswas <biswasbijoy414@gmail.com>
2022-02-07 23:40:52 +0600

    user list update

--- lib/Screens/calls_screen.dart
+++ lib/Screens/calls_screen.dart
@@ -1,3 +1,4 @@
+import 'package:airmail/Model/chat_model.dart';
 import 'package:flutter/material.dart';
 
 class CallScreen extends StatefulWidget {
@@ -10,27 +11,67 @@ class CallScreen extends StatefulWidget {
 class _CallScreenState extends State<CallScreen> {
   @override
   Widget build(BuildContext context) {
+    List<ChatModel> calls = [
+      ChatModel(
+        name: 'Bill Gates',
+        icon: "assets/images/BillGates.jpeg",
+      ),
+      ChatModel(
+        name: 'Boris Johnson',
+        icon: "assets/images/BorisJohnson.jpg",
+      ),
+      ChatModel(
+        name: 'Donald Trump',
+        icon: "assets/images/DonaldTrump.jpg",
+      ),
+      ChatModel(
+        name: 'Recep Tayyip Erdogan',
+        icon: "assets/images/Erdogan.jpg",
+      ),
+      ChatModel(
+        name: 'Jack Ma',
+        icon: "assets/images/JackMa.jpg",
+      ),
+      ChatModel(
+        name: 'Joe Biden',
+        icon: "assets/images/JoeBiden.jpg",
+      ),
+      ChatModel(
+        name: 'John Abrahm',
+        icon: "assets/images/JohnAbraham.jpg",
+      ),
+      ChatModel(
+        name: 'Steve Jobs',
+        icon: "assets/images/SteveJobs.jpg",
+      ),
+      ChatModel(
+        name: 'Sundor Pichai',
+        icon: "assets/images/SundorPichai.jpg",
+      ),
+      ChatModel(
+        name: 'Vladimir Putin',
+        icon: "assets/images/VladimirPutin.jpg",
+      )
+    ];
     return Scaffold(
-      body: ListView(
-        children: [
-          callCard(
-            'Bill Gates',
-            Icons.call_end,
-            Colors.red,
-            '5:00 PM',
-          ),
-        ],
+      body: ListView.builder(
+        itemCount: calls.length,
+        itemBuilder: (context, index) {
+          return callCard(calls[index].name, Icons.call_missed_outgoing,
+              Colors.red, "10:00 AM", calls[index].icon);
+        },
       ),
     );
   }
 
-  Widget callCard(String name, IconData icon, Color color, String time) {
+  Widget callCard(
+      String name, IconData icon, Color color, String time, String img) {
     return Card(
       margin: EdgeInsets.only(bottom: 0.5),
       child: ListTile(
         leading: CircleAvatar(
           radius: 25,
-          child: Icon(Icons.person),
+          backgroundImage: AssetImage(img),
         ),
         title: Text(name,
             style: TextStyle(fontSize: 19, fontWeight: FontWeight.w500)),
