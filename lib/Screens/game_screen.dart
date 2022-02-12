a7d84aaa8b66009f7c74d68ae827b9a0945e3484
Bijoy Biswas <biswasbijoy414@gmail.com>
2022-02-12 16:24:54 +0600

    game screen update

--- lib/Screens/game_screen.dart
+++ lib/Screens/game_screen.dart
@@ -1,3 +1,6 @@
+import 'package:airmail/Games/Color_Guess/guess_color.dart';
+import 'package:airmail/Games/Rock_Paper_Scissor/rock_paper_scissor.dart';
+import 'package:airmail/Games/Tic_Tac_Toe/tic_tac_toe.dart';
 import 'package:flutter/material.dart';
 
 class GameScreen extends StatefulWidget {
@@ -13,47 +16,65 @@ class _GameScreenState extends State<GameScreen> {
     return Scaffold(
       body: ListView(
         children: [
-          Card(
-            margin: EdgeInsets.all(8),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            elevation: 5,
-            child: CardContants('assets/images/rock_paper_scissor.png'),
+          InkWell(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => RockPaperScissor()));
+            },
+            child: CardContants(
+                "assets/images/rock_paper_scissor.png", "Rock Paper Scissor"),
           ),
-          Card(
-            margin: EdgeInsets.all(8),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            elevation: 5,
-            child: CardContants('assets/images/tic_tac_toe.png'),
-          ),
-          Card(
-            margin: EdgeInsets.all(8),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            elevation: 5,
-            child: CardContants('assets/images/color_guessing.png'),
-          ),
+          InkWell(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => TicTacToy()));
+              },
+              child:
+                  CardContants("assets/images/tic_tac_toe.png", "Tic Tac Toe")),
+          InkWell(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => ColorGuess()));
+              },
+              child: CardContants(
+                  "assets/images/color_guessing.png", "Guess the Color")),
         ],
       ),
     );
   }
 
-  Widget CardContants(String img) {
-    return Container(
-      height: MediaQuery.of(context).size.height * 0.3,
-      // color: Colors.green,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        image: DecorationImage(
-          image: AssetImage(img),
-          fit: BoxFit.cover,
+  Widget CardContants(String img, String txt) {
+    return Stack(
+      children: [
+        Container(
+          margin: EdgeInsets.all(10),
+          height: MediaQuery.of(context).size.height * 0.3,
+          // color: Colors.green,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            image: DecorationImage(
+              image: AssetImage(img),
+              fit: BoxFit.cover,
+            ),
+          ),
         ),
-      ),
+        Positioned.fill(
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                txt,
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
