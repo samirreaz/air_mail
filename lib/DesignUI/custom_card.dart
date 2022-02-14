07ce5111cfc1a96cbe34da18b47c64ced2a70cd4
Bijoy Biswas <biswasbijoy414@gmail.com>
2022-02-14 23:21:47 +0600

    passing data parent class to child class

--- lib/DesignUI/custom_card.dart
+++ lib/DesignUI/custom_card.dart
@@ -3,19 +3,25 @@ import 'package:airmail/Screens/indivisual_page.dart';
 import 'package:flutter/material.dart';
 
 class CustomCard extends StatelessWidget {
-  const CustomCard({Key? key, required this.chatModel}) : super(key: key);
+  const CustomCard(
+      {Key? key, required this.chatModel, required this.sourceChat})
+      : super(key: key);
   final ChatModel chatModel;
+  final ChatModel sourceChat;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
         Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => IndivisualPage(
-                      chatModel: chatModel,
-                    )));
+          context,
+          MaterialPageRoute(
+            builder: (context) => IndivisualPage(
+              chatModel: chatModel,
+              sourceChat: sourceChat,
+            ),
+          ),
+        );
       },
       child: Column(
         children: [
@@ -24,18 +30,26 @@ class CustomCard extends StatelessWidget {
               radius: 30,
               backgroundImage: AssetImage(chatModel.icon),
             ),
-            title: Text(chatModel.name,
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+            title: Text(
+              chatModel.name,
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
             subtitle: Row(
               children: [
-                Icon(Icons.done_all, color: Colors.blue, size: 16),
+                Icon(Icons.done_all, color: Colors.white, size: 16),
                 Text(
                   chatModel.currentMessage,
-                  style: TextStyle(fontSize: 16),
+                  style: TextStyle(fontSize: 16, color: Colors.white),
                 ),
               ],
             ),
-            trailing: Text(chatModel.time),
+            trailing: Text(
+              chatModel.time,
+              style: TextStyle(color: Colors.white),
+            ),
           ),
         ],
       ),
