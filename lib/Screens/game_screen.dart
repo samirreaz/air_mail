ffb58a3ce2cf8e3779261cb3767427a90298e3da
Bijoy Biswas <biswasbijoy414@gmail.com>
2022-02-14 23:26:46 +0600

    update background color

--- lib/Screens/game_screen.dart
+++ lib/Screens/game_screen.dart
@@ -14,6 +14,7 @@ class _GameScreenState extends State<GameScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.indigoAccent,
       body: ListView(
         children: [
           InkWell(
