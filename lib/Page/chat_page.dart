ab4933f3b955475f859bf2d984fce644473870fa
Bijoy Biswas <biswasbijoy414@gmail.com>
2022-02-14 23:25:35 +0600

    call parent class and delete ChatModel list

--- lib/Page/chat_page.dart
+++ lib/Page/chat_page.dart
@@ -4,83 +4,28 @@ import 'package:airmail/Screens/select_contact_page.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatefulWidget {
-  const ChatPage({Key? key}) : super(key: key);
+  const ChatPage({Key? key, required this.chatmodels, required this.sourceChat})
+      : super(key: key);
+  final List<ChatModel> chatmodels;
+  final ChatModel sourceChat;
 
   @override
   State<ChatPage> createState() => _ChatPageState();
 }
 
 class _ChatPageState extends State<ChatPage> {
-  List<ChatModel> chats = [
-    ChatModel(
-        name: 'Bill Gates',
-        icon: "assets/images/BillGates.jpeg",
-        time: "4.00",
-        currentMessage: "Hi"),
-    ChatModel(
-        name: 'Boris Johnson',
-        icon: "assets/images/BorisJohnson.jpg",
-        time: "4.00",
-        currentMessage: "Hello, How are you?"),
-    ChatModel(
-        name: 'Donald Trump',
-        icon: "assets/images/DonaldTrump.jpg",
-        time: "4.00",
-        currentMessage: "Nice, I am fine"),
-    ChatModel(
-        name: 'Recep Tayyip Erdogan',
-        icon: "assets/images/Erdogan.jpg",
-        time: "4.00",
-        currentMessage: "Hey, Whats up?"),
-    ChatModel(
-        name: 'Jack Ma',
-        icon: "assets/images/JackMa.jpg",
-        time: "4.00",
-        currentMessage: "What About You?"),
-    ChatModel(
-        name: 'Joe Biden',
-        icon: "assets/images/JoeBiden.jpg",
-        time: "4.00",
-        currentMessage: "Nice to meet you"),
-    ChatModel(
-        name: 'John Abrahm',
-        icon: "assets/images/JohnAbraham.jpg",
-        time: "4.00",
-        currentMessage: "Nice to meet you"),
-    ChatModel(
-        name: 'Steve Jobs',
-        icon: "assets/images/SteveJobs.jpg",
-        time: "4.00",
-        currentMessage: "Nice to meet you"),
-    ChatModel(
-        name: 'Sundor Pichai',
-        icon: "assets/images/SundorPichai.jpg",
-        time: "4.00",
-        currentMessage: "Nice to meet you"),
-    ChatModel(
-        name: 'Vladimir Putin',
-        icon: "assets/images/VladimirPutin.jpg",
-        time: "4.00",
-        currentMessage: "Nice to meet you"),
-  ];
   @override
   Widget build(BuildContext context) {
+    // ChatModel sourceChat = chatmodels.removeAt(0);
     return Scaffold(
-      // floatingActionButton: FloatingActionButton(
-      //   child: Icon(Icons.chat),
-      //   onPressed: () {
-      //     Navigator.push(context, MaterialPageRoute(builder: (context) {
-      //       return SelectContact();
-      //     }));
-      //   },
-      // ),
+      backgroundColor: Colors.indigoAccent,
       body: ListView.builder(
-          itemCount: chats.length,
-          itemBuilder: (BuildContext context, int index) {
-            return CustomCard(
-              chatModel: chats[index],
-            );
-          }),
+        itemCount: widget.chatmodels.length,
+        itemBuilder: (context, index) => CustomCard(
+          chatModel: widget.chatmodels[index],
+          sourceChat: widget.sourceChat,
+        ),
+      ),
     );
   }
 }
